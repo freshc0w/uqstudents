ee838be83bbb2f4058daba17203b4221fd9feb97
freshc0w <121275444+freshc0w@users.noreply.github.com>
2024-08-29 17:54:44 +1000

    Add mode toggle for themes and configure themes:

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import "./App.css";
 import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <>
+      <ModeToggle />
       <Button>Testing</Button>
     </>
   );
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from '@/components/theme-provider.tsx'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
