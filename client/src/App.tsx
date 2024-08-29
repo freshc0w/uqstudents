f64d38d530a151cff54bf10f91c763a8577a8577
freshc0w <121275444+freshc0w@users.noreply.github.com>
2024-08-29 17:02:38 +1000

    Initialise client side with shadcn

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button>Testing</Button>
+    </>
+  );
+}
+
+export default App;
