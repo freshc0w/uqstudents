bd030bb261df8a8ee22e88737f791445773250f8
freshc0w <121275444+freshc0w@users.noreply.github.com>
2024-09-07 16:39:10 +1000

    Use CourseInfo type on course route

--- client/src/app/routes/app/reviews/courses/course.tsx
+++ client/src/app/routes/app/reviews/courses/course.tsx
@@ -1,15 +1,18 @@
 import { ReviewLayout } from "@/components/layouts/review-layout";
-import { H1, H2, Muted, P } from "@/components/typography";
+import { H1, H2, P } from "@/components/typography";
 import { useParams } from "react-router-dom";
 import courseInfos from "@/features/reviews/courses/assets/course-info.json"; // ! TEMPORARY
 import BoxReveal from "@/components/ui/box-reveal";
 import { CourseHeaderInfo } from "@/features/reviews/courses/components/course-header-info";
+import { CourseInfo } from "@/features/reviews/courses/types";
 
 // TODO: Implement Head for SEO
 export const CourseRoute = () => {
   let { courseCode } = useParams();
   courseCode = courseCode?.toUpperCase();
-  const courseInfo = courseInfos.find((course) => course.code === courseCode);
+  const courseInfo = (courseInfos as CourseInfo[]).find(
+    (course) => course.code === courseCode,
+  );
 
   // TODO: Do the finding of the course, for now we'll use static
   if (!courseInfo) {
