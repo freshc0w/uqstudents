622006436e77fd5f11bb4eca9dfbe9c72636d051
freshc0w <121275444+freshc0w@users.noreply.github.com>
2024-09-08 19:27:03 +1000

    Refactor ActionableIcon and refactor layout for responsiveness for review component

--- client/src/features/reviews/courses/components/review.tsx
+++ client/src/features/reviews/courses/components/review.tsx
@@ -1,13 +1,15 @@
 import { H4, Lead, P, Small } from "@/components/typography";
 import { ActionableIcon } from "@/features/reviews/courses/components/actionable-icon";
 import { RatingChartComponent } from "@/features/reviews/courses/components/rating-chart-component";
-import { ThumbsUp, ThumbsDown } from "lucide-react";
+import { ThumbsUp, ThumbsDown, MessageSquareText } from "lucide-react";
 
+{
+  /* TODO: Add REAL REVIEW values as props. */
+}
 export const Review = () => {
   return (
     <div className="py-2 flex">
       <div className="p-2 md:p-3 grid place-items-center">
-        {/* TODO: Add the REAL rating value */}
         <RatingChartComponent value={Math.floor(Math.random() * 100)} />
       </div>
       <div className="flex-1 space-y-2">
@@ -21,21 +23,29 @@ export const Review = () => {
           modi totam numquam exercitationem, laboriosam architecto consectetur,
           distinctio eos natus maxime optio sunt cum quasi error repellat?
         </P>
-        <div className="flex gap-3 items-center">
-          <Small className="text-secondary-foreground italic">08/09/2024</Small>
-          <Lead className="text-accend sm:text-sm">
-            Taken on Semester 1, 2024
-          </Lead>
-        </div>
-        <div className="flex items-center justify-end gap-4">
-          <ActionableIcon>
-            <ThumbsUp size={20} />
-            <Small>123</Small>
-          </ActionableIcon>
-          <ActionableIcon>
-            <ThumbsDown size={20} />
-            <Small>123</Small>
-          </ActionableIcon>
+        <div className="flex justify-between flex-col items-start md:flex-row md:items-center">
+          <div className="flex gap-3 items-center">
+            <Small className="text-secondary-foreground italic">
+              08/09/2024
+            </Small>
+            <Lead className="text-accend text-xs sm:text-sm">
+              Taken on Semester 1, 2024
+            </Lead>
+          </div>
+          <div className="flex items-center justify-end gap-4">
+            <ActionableIcon>
+              <ThumbsUp size={20} />
+              <Small>123</Small>
+            </ActionableIcon>
+            <ActionableIcon>
+              <ThumbsDown size={20} />
+              <Small>123</Small>
+            </ActionableIcon>
+            <ActionableIcon>
+              <MessageSquareText size={20} />
+              <Small>123</Small>
+            </ActionableIcon>
+          </div>
         </div>
       </div>
     </div>
