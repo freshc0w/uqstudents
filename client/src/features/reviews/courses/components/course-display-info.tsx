9f427cc04c6d0483d7c2241d2fa2b0179e5a981a
freshc0w <121275444+freshc0w@users.noreply.github.com>
2024-09-09 19:26:04 +1000

    Add more information and compartmentalise a lot of logic in CourseDisplaYInfo component

--- client/src/features/reviews/courses/components/course-display-info.tsx
+++ client/src/features/reviews/courses/components/course-display-info.tsx
@@ -2,24 +2,95 @@ import { Large, Small } from "@/components/typography";
 import { Ratings } from "@/components/ui/ratings";
 import { Separator } from "@/components/ui/separator";
 import { CourseInfo } from "@/features/reviews/courses/types";
+import courseMap from "../assets/faculty.json";
+import { cn } from "@/lib/utils";
+import { TooltipProvider, Tooltip } from "@radix-ui/react-tooltip";
+import { TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import BlurIn from "@/components/ui/blur-in";
+
+const BoldHighlightText = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => (
+  <Large
+    className={cn(
+      "inline text-complementary sm:text-xl md:text-2xl leading-none sm:leading-none md:leading-none lg:leading-none",
+      className,
+    )}
+  >
+    {children}
+  </Large>
+);
+
+const InfoDisplay = ({
+  label,
+  info,
+  className,
+}: {
+  label: string;
+  info: string;
+  className?: string;
+}) => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger>
+        <div className="flex items-center gap-2 cursor-default">
+          <Small className="italic text-sm sm:text-xs">{label}</Small>
+          <BoldHighlightText
+            className={cn("text-sm sm:text-md md:text-lg", className)}
+          >
+            {info}
+          </BoldHighlightText>
+        </div>
+      </TooltipTrigger>
+      <TooltipContent>
+        <Small>{info}</Small>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
 
 type CourseInfoProps = {
   courseInfo: CourseInfo;
 };
 
 export const CourseDisplayInfo = ({ courseInfo }: CourseInfoProps) => {
-  return (
-    <div className="flex gap-4 justify-around">
-      <Ratings rating={3.5} variant="complementary" readOnly noText size={25} />
-      <Separator orientation="vertical" className="w-[2px]" />
-      <Small className="font-light">
-        <Large className="inline text-complementary sm:text-xl md:text-2xl">
-          98
-        </Large>{" "}
-        average of <span className="font-bold italic">35</span> reviews
-      </Small>
-    </div>
+  const getFaculty = (faculty: string) => {
+    const abbreviatedFaculty =
+      courseMap[faculty.trim() as keyof typeof courseMap];
 
-    
+    return abbreviatedFaculty ?? faculty.trim();
+  };
+  return (
+    <>
+      <BlurIn duration={0.75}>
+        <div className="flex gap-2 justify-around">
+          <Ratings
+            rating={3.5}
+            variant="complementary"
+            readOnly
+            noText
+            size={25}
+          />
+          <Separator orientation="vertical" className="w-[1.5px]" />
+          <span className="flex gap-1">
+            <BoldHighlightText>98</BoldHighlightText>{" "}
+            <Small className="flex items-center gap-1">
+              average of <i className="font-bold">35</i> reviews
+            </Small>
+          </span>
+        </div>
+        <div className="flex gap-2 justify-around">
+          <InfoDisplay label="Faculty" info={getFaculty(courseInfo.faculty)} />
+          <Separator orientation="vertical" className="w-[1.5px]" />
+          <InfoDisplay label="Level" info={courseInfo.level} />
+          <Separator orientation="vertical" className="w-[1.5px]" />
+          <InfoDisplay label="Mode" info={courseInfo.mode} />
+        </div>
+      </BlurIn>
+    </>
   );
 };
